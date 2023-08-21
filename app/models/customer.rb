56dea56eb83e8e03623c32b12cb4e9865811b680
JVD-Leif <l.bersamina@jvd.com.ph>
2023-08-21 09:37:49 +0800

    add initial model associations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
 class Customer < ApplicationRecord
+  has_many :orders, foreign_key: 'customer_id'
+  has_many :products, through: :orders
+  has_one :billing_detail
+  has_many :comments, foreign_key: 'user_id'
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  has_many :orders
+  has_many :customers, through: :orders
+  has_many :comments
 end
