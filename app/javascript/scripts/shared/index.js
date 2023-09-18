5ae1666258d559cb3df49737942c3e7dc49c7052
JVD-Leif <l.bersamina@jvd.com.ph>
2023-09-18 23:11:45 +0800

    add dynamic header style and adjust top margins for pages

---
+++ app/javascript/scripts/shared/index.js
@@ -0,0 +1,13 @@
+const nav = document.getElementById('nav')
+
+console.log(nav)
+
+window.addEventListener('scroll', (e) => {
+  if(window.scrollY > 10) {
+    nav.classList.add('bg-white')
+    nav.classList.add('text-gray-700')
+  } else {
+    nav.classList.remove('bg-white')
+    nav.classList.remove('text-gray-700')
+  }
+})
