e333875dff52128c89459ca119b913b195c8e93e
JVD-Leif <l.bersamina@jvd.com.ph>
2023-09-21 23:46:55 +0800

    add initial home page with dynamic header/nav

--- app/javascript/scripts/shared/index.js
+++ app/javascript/scripts/shared/index.js
@@ -1,12 +1,11 @@
 const nav = document.getElementById('nav')
 
-
 window.addEventListener('scroll', (e) => {
-  if(window.scrollY > 10) {
+  if(window.scrollY > 20) {
     nav.classList.add('bg-white')
-    nav.classList.add('text-gray-700')
+    nav.classList.add('text-black')
   } else {
     nav.classList.remove('bg-white')
-    nav.classList.remove('text-gray-700')
+    nav.classList.remove('text-black')
   }
 })
