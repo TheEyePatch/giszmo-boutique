47c561d45a19b9bc9a9080b73dda29520933e969
JVD-Leif <l.bersamina@jvd.com.ph>
2023-12-02 21:43:06 +0800

    Add admin update modal and functionality
    Feature includes edit form modal and update method controller with turbo stream
    to dynamically update admin data on index page table.

--- app/controllers/admin_console/admins_controller.rb
+++ app/controllers/admin_console/admins_controller.rb
@@ -8,6 +8,29 @@ module AdminConsole
       @admin = Admin.new
     end
 
+    def edit
+      @admin = admin
+    end
+
+    def update
+      @admin = admin
+
+      if admin.update admin_params.except(:password, :password_confirmation)
+        respond_to do |format|
+          format.html
+          format.turbo_stream do
+            flash.now[:notice] = 'Successfully update Admin'
+            render __method__, locals: { admin: @admin }
+          end
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = @admin.errors.full_messages
+          render __method__, locals: { admin: nil }
+        end
+      end
+    end
+
     def create
       @admin = Admin.new admin_params
 
@@ -16,7 +39,7 @@ module AdminConsole
           format.html
           format.turbo_stream do
             flash.now[:notice] = 'Successfully created Admin'
-            render :create, locals: { admin: @admin }
+            render __method__, locals: { admin: @admin }
           end
         end
       else
@@ -24,7 +47,7 @@ module AdminConsole
           format.html
           format.turbo_stream do
             flash.now[:alert] = @admin.errors.full_messages
-            render :create, locals: { admin: nil }
+            render __method__, locals: { admin: nil }
           end
         end
       end
